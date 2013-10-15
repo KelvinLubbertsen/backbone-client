5bd838c0350fb393f67c0fd00932baa4921b383b
kjm.lubbertsen@gmail.com <kjm.lubbertsen@gmail.com@9ab5bb88-e078-e823-4ede-1df481c36ca6>
2013-10-15 13:22:36 +0000

    update 2

--- trunk/script.js
+++ trunk/script.js
@@ -1,15 +1,15 @@
 (function($) {
 	
 	Table = Backbone.Model.extend({
+		defaults: {
 		name: null,
 		reserved: false,
-		orders: []
+		orders: []}
 	});
 	
 	Tables = Backbone.Collection.extend({
 		
-		initialize: function(models, options){
-			this.bind("add", options.view.addTable);
+		initialize: function(){
 		},
 		
 		localStorage: new Backbone.LocalStorage('tables-backbone'),
@@ -18,34 +18,19 @@
 			return table.reserved;
 		},
 		
-		freeTables: function() {
+		free: function() {
 			//return this.without.apply(this, this.freeTable);
 			
-			return this.filter(function(table) {return table.reserved});
-		}
-	});
-	
-	AppView = Backbone.View.extend({
-		el: $('body'),
-		initialize: function(){
-			this.tables = new Tables(null, {view: this});
+			return this.where({reserved: false});
 		},
-		events: {
-			"click #submit": "submitTable", 
-		},
-		submitTable: function(){
-			var table = $('#table-name').val();
-			var table_model = new Table({name: table});
-			this.tables.add(table_model);
-			console.log(this.tables);
-		},	
-		addTable: function(model){
-			$('#tables').append("<li>" + model.get('name') + "</li>");
+		
+		find: function(name) {
+			return this.where({name: name});
 		}
 	});
 	
 	KlantView = Backbone.View.extend({
-		el: $('#tables-book-list'),
+		el: $('#klant-view'),
 		
 		initialize: function(){
 			this.model = new Tables(null, {view: this});
@@ -57,9 +42,30 @@
 			this.model.add(new Table({name: "tafel 3"}));
 			console.log(this.model);
 		},
-		template: _.template($('#klant-view-dropdown-template').html()),
+		template: _.template($('#klant-view-template').html()),
 		events: {
+			"click #submit-booking": "submitBooking"
 		},
+		submitBooking: function(data)
+		{
+			var options = $('#tables-book-list option');
+			var name;
+			options.each(function(key, item) {
+				if(item.selected)
+				{
+					name = $(item).text();
+					
+				
+				}
+			});
+			var item = this.model.find(name);
+			item = item[0];
+			console.log(item);
+			item.set({reserved: true});
+
+			$('#klant-view-message').text("You booked " + item.get('name') + "!");
+		}
+		,
 		render: function() {
 			/*for(var i = 0; i < this.model.length; i++)
 			{
@@ -68,18 +74,41 @@
 				$('#tables-book-list').append(option);
 				console.log('<option>' + this.model.i.get('name') + '</option>');
 			}*/
-			$('#tables-book-list').html('');
-			console.log(this.model.get);
-			this.model.get('freeTables').each(function(item) {
+			//$('#tables-book-list').html('');
+			
+			/*$(this.model.free()).each(function(key,item) {
 				var option = $('<option>');
 				option.text(item.get('name'));
+				option.addClass('dropdown-item');
+				option.val(item.cid);
 				$('#tables-book-list').append(option);
+			});*/
+			
+			$(this.model.free()).each(function(key,item) {
+				var view = new OptionRowView({model: item});
+				$('#tables-book-list').append(view.$el);
 			});
 			
 			return this;
 		}
 		
 	});
+	
+	OptionRowView = Backbone.View.extend({
+		_template: _.template($('#klant-view-dropdown-template').html()),
+		
+		initialize: function() {this.render();},
+		
+		render: function() {
+			this.$el.html(this._template(this.model.toJSON()));
+			return this;
+		}
+	});
+	
+	MedewerkerView = Backbone.View.extend({
+		
+	});
+	
 	var appview;
 	
 	Router = Backbone.Router.extend({
@@ -92,13 +121,12 @@
 		medewerker: function() {
 			/*$('#medewerker-view').show();
 			$('#klant-view').hide();*/
-			appview = new KlantView;
+			appview = new MedewerkerView;
 		},
 		klant: function() {
 			/*$('#medewerker-view').hide();
 			$('#klant-view').show();*/
 			appview = new KlantView;
-			console.log('Klant!');
 		},
 	});
 	
